f74e26a21f390917740588f2dca503c742d81832
Lim Kiat <limkiat@u.nus.edu>
2016-10-22 14:17:07 -0700

    Xcode Project creation initial commit

---
+++ HiFive/HiFive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HiFive
+//
+//  Created by Lim Kiat on 22/10/16.
+//  Copyright © 2016 Sahara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
